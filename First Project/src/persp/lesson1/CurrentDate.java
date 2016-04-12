d3df7f62bedb5c7f723697ad293a40c08b628c06
Paul25 <pavel.marchenko.6583@gmail.com>
2016-04-12 18:05:20 +0300

    added files

--- First Project/src/persp/lesson1/CurrentDate.java
+++ First Project/src/persp/lesson1/CurrentDate.java
@@ -7,14 +7,26 @@ public class CurrentDate {
 		final long millisInAday = 24 * 60 * 60 * 1000;
 		final int startYear=1970;
 		final int daysInAyear=365;
+		final int daysInMonth=30;
 		final int monthInAyear=12;
+		final int leapDays=11;
 						
 		long timeMs=System.currentTimeMillis();
-		long daysFromStart=timeMs/millisInAday; 
+		System.out.println(timeMs);
+		long daysFromStart=timeMs/millisInAday;
+		System.out.println(daysFromStart);
 		long currentYear=startYear + daysFromStart/daysInAyear;
+		System.out.println(currentYear);
 		long quantityYears=currentYear-startYear;
+		System.out.println(quantityYears);
 		long day=daysFromStart/quantityYears/daysInAyear+1;
+		long day1=((daysFromStart-quantityYears*daysInAyear)/daysInMonth)+(leapDays-10)+1;
+		System.out.println(day1);
 		long currentMonth=quantityYears/monthInAyear;
+		long currentMonth1=((daysFromStart/daysInMonth)/monthInAyear)/monthInAyear;
+		System.out.println(currentMonth1);
+		System.out.println(day1+"."+currentMonth1+"."+currentYear);
+		System.out.println(day+"."+currentMonth+"."+currentYear);
 			
 		int month = (int) currentMonth;
         switch(month){
---
+++ First Project/src/persp/lesson1/MyBoxMain.java
@@ -0,0 +1,61 @@
+package persp.lesson1;
+
+//import org.w3c.dom.css.Counter;
+
+public class MyBoxMain {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//        MyBox box=new MyBox();
+//        System.out.println(MyBox.counter);
+//        //box.colour="What"; <-private
+//        box.price=100;
+//        // box.weight=60;
+//        MyBox oldBox=new MyBox("green");
+//        System.out.println(MyBox.counter);
+//        MyBox firstBox=new MyBox(50,"Yellow", false);
+//        System.out.println(MyBox.counter);
+//        System.out.println(firstBox.height);
+//        //System.out.println(box.colour+" "+firstBox.weight+" "+firstBox.isOpen);<-private
+//        box.getInfo();
+//        oldBox.getInfo();
+//        box.setColour("grey");
+//        //box.colour="Null";<-private
+//        box.setParam("blue", 20);
+//        box.getInfo();
+//        box.getColour(); 
+//        System.out.println(box.getColour());
+//        String col=box.getColour();
+//        System.out.println(col);
+//        MyBox storage[]=new MyBox[20];
+//        for (int i = 0; i < storage.length; i++) {
+//        	int colourIndex= (int) (Math.random()*4);
+//        	String c= MyBox.colours[colourIndex];
+//        	int w=(int) (Math.random()*100);
+//        	boolean bool=Math.random()>0.5?true:false;
+//        	MyBox b=new MyBox(w, c, bool);
+//     	b.getInfo();
+// //       	storage[i]=b;
+//            storage[i]  = new MyBox((int) (Math.random()*100), MyBox.colours[(int) (Math.random()*4)], Math.random()>0.5?true:false);            	
+//        	
+//        	System.out.println(storage[i]);
+//			
+//		}
+//        for (int i = 0; i < storage.length; i++) {
+//        	storage[i].getInfo();
+//			
+//        MyBox [] boxes=MyBox.fillBoxes(15);
+////        for (int j = 0; j < boxes.length; j++) {
+////        	boxes[j].getInfo();
+//			MyBox.getAllInfo(boxes);
+//			
+//        
+//		}
+//        
+//        
+//       }
+	}
+
+	
+
+
